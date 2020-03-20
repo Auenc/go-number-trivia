1e98ef0a6638b3dbfe0d477aae2a30db28725ab9
Lewis <ljoncb@gmail.com>
2020-03-20 21:52:26 +0000

    Usecase number trivia repository
    The usecase for the number trivia repository.

--- usercases/NumberTriviaRepository.go
+++ usercases/NumberTriviaRepository.go
@@ -2,6 +2,10 @@ package usercases
 
 import "github.com/auenc/go-number-trivia/domain/model"
 
+const (
+	ErrorNumberIsNegative = "number is negative. number trivia only accepts positive numbers"
+)
+
 //NumberTriviaRepository is the interface that defines the two ways of obtaining NumberTrivia
 type NumberTriviaRepository interface{
 	Specific(int) (model.NumberTrivia, error)
