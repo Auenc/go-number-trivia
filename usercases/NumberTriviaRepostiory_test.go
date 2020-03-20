df681ddb3d08d8523afb2d1b47d41fd1059e366f
Lewis <ljoncb@gmail.com>
2020-03-20 21:53:24 +0000

    Testing the functionality of NumberTriviaRepository specific
    Specific is expected to return the trivia for a number provided it is positive. If the number is negative, then the repository should return an ErrorNumberIsNegative error

---
+++ usercases/NumberTriviaRepostiory_test.go
@@ -0,0 +1,53 @@
+package usercases
+
+import (
+	"errors"
+	"github.com/auenc/go-number-trivia/domain/model"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//mockNumberTriviaRepository is the fake repository to test the interface
+type mockNumberTriviaRepository struct{
+}
+//Specific is the mocked method of NumberTriviaRepository.Specific
+func (r *mockNumberTriviaRepository) Specific(number int) (*model.NumberTrivia, error){
+	if number < 0{
+		return nil, errors.New(ErrorNumberIsNegative)
+	}
+	return &model.NumberTrivia{
+		Number: number,
+		Text:   "some test trivia",
+	}, nil
+}
+
+func (r *mockNumberTriviaRepository) Random() (*model.NumberTrivia, error){
+	return &model.NumberTrivia{
+		Number: 1,
+		Text:  "some random test trivia",
+	}, nil
+}
+
+func TestNumberTriviaRepository_Specific(t *testing.T){
+	t.Run("positive number", func(t *testing.T) {
+		repo := new(mockNumberTriviaRepository)
+
+		expected := &model.NumberTrivia{
+			Number: 1,
+			Text:   "some test trivia",
+		}
+
+		result, err := repo.Specific(1)
+		assert.Nil(t, err)
+		assert.Equal(t, expected,result)
+	})
+	t.Run("negative number", func(t *testing.T) {
+		repo := new(mockNumberTriviaRepository)
+		expected := errors.New(ErrorNumberIsNegative)
+		shouldBeNil, result := repo.Specific(-1)
+		assert.Nil(t, shouldBeNil)
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+}
+
